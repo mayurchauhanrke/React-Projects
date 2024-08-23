b8517468e9b3bcb685e40a37970fbae78ab3004d
Mayur Chauhan <109025933+mayurchauhanrke@users.noreply.github.com>
2024-08-23 15:08:37 +0530

    Add files via upload

---
+++ NikeBrandPage/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Navigation from './components/Navigation';
+import Hero from './components/Hero';
+
+function App() {
+
+  return (
+    <div>
+      <Navigation/>
+      <Hero/>
+    </div>
+  )
+}
+
+export default App;
